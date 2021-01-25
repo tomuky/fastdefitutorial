dd54bf86d99796719db6e3d073ca8a80942229ce
Tom <mrtomd@gmail.com>
2021-01-25 11:21:01 -0500

    adding initial files

---
+++ index.php
@@ -0,0 +1,295 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171278527-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-171278527-1');
+    </script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Fast DeFi Tutorial</title>
+    <meta name="description" content="Fast DeFi Tutorial">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>">
+    <meta name="author" content="Tom">
+    <link rel="stylesheet" href="css/style.4.css?v=1">
+    <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="https://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+		
+    <meta property="og:url"                content="http://fastdefitutorial.com" />
+    <meta property="og:title"              content="Fast DeFi Tutorial" />
+    <meta property="og:description"        content="The fastest DeFi tutorial on Earth" />
+    <meta property="og:image"              content="images/bolt-fb2.png" />
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@fastdefi">
+    <meta name="twitter:creator" content="@tomuky">
+    <meta name="twitter:title" content="Fast DeFi Tutorial">
+    <meta name="twitter:description" content="The fastest DeFi tutorial on Earth" >
+    <meta name="twitter:image" content="http://fastdefitutorial.com/images/bolt-tw.png" />
+  </head>
+
+  <body>
+    <script src="js/utility.0.js"></script>
+    <script src="js/script.0.js"></script>
+
+    <div class='main'>
+      
+      <a href='/' class='title-area'>
+        <h1>The Fastest DeFi Tutorial on Earth</h1>
+      </a>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2>Overview</h2>
+        <p class='center'>I made a list of steps straight to DeFi</p>
+				
+        <div class='center'>
+          <ul class='larger'>
+            <li class='hashtag'><a href='#what-is-defi'>What is DeFi 🤔</a></li>
+            <li class='hashtag'><a href='#must-knows'>The must-knows 💡</a></li>
+            <li class='hashtag'><a href='#get-a-wallet'>First: Get a wallet 💰</a></li>
+            <li class='hashtag'><a href='#check-gas-prices'>Check gas prices ⛽</a></li>
+            <li class='hashtag'><a href='#earn-on-compound'>Earn interest on Compound 📈</a></li>
+            <li class='hashtag'><a href='#swap-on-uniswap'>Swap tokens on Uniswap 🦄</a></li>
+            <li class='hashtag'><a href='#track-your-portfolio'>Track your DeFi portfolio 📊</a></li>
+            <li class='hashtag'><a href='#play-no-loss-lottery'>Play a no-loss lottery 🎟</a></li>
+            <li class='hashtag'><a href='#save-with-mstable'>Save with mStable 🧮</a></li>
+            <li class='hashtag'><a href='#supply-to-uniswap'>Earn fees on Uniswap 🦄</a></li>
+            <li class='hashtag'><a href='#track-il'>Track liquidity on APY.Vision 👀</a></li>
+            <li class='hashtag'><a href='#buy-dpi'>Invest in DeFi with one token 💲</a></li>
+				  </ul>
+        </div>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='what-is-defi'>What is DeFi 🤔</h2>
+        <ul>
+          <li class='i'>Short for Decentralized Finance</li>
+          <li class='i'>Financial products and services built as immutable verifiable code</li>
+					<li class='i'>Most of DeFi is built on the Ethereum blockchain</li>
+					<li class='i'>No intermediaries or middle-men</li>
+					<li class='i'>Available to anyone in the world with an internet connection</li>
+        </ul>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='must-knows'>The must-knows 💡</h2>
+        <ul>
+          <li>Always try things with small amounts of money before using larger amounts</li>
+          <li>You need ether (ETH) to pay for gas to transact on the Ethereum network</li>
+          <li>There are gas fees on transactions that will negate any positive yield in DeFi when using small amounts of money, but it is a safe way to learn before using larger amounts of money</li>
+					<li>Make sure network <a href='#check-gas-prices'>gas prices</a> aren't too high for trying things</li>
+					<li>Never share your seed phrase or store them on your phone or computer. Write it down on paper!</li>
+          <li>Only send ETH to an ETH address, bitcoin to a bitcoin addresses, etc!</li>
+          <li>However, you can send and receive <a href='https://etherscan.io/tokens' target='_blank'>ERC20 tokens</a> with an Ethereum address since they are programmed on the Ethereum network</li>
+        </ul>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='get-a-wallet'>First: Get a wallet 💰</h2>
+				<p class='center'>Start here. You need a wallet to interact with DeFi protocols.<p>
+				
+				<ol>
+					<li>Get a <a href='https://www.coinbase.com/join/davis_95' target='_blank'>Coinbase</a> account</li>
+          <li>Connect your checking account</li>
+          <li>Buy $10-$50 worth of Ethereum (make take some days for funds to clear bank)</li>
+					<li>Download <a href="https://metamask.io/download.html" target='_blank'>MetaMask mobile app</a> (DeFi on mobile)</li>
+					<li>Choose Create a new wallet</li>
+					<li>Follow on-screen instructions, write down seed phrase on paper</li>
+					<li>Download <a href="https://metamask.io/download.html" target="_blank">MetaMask Google Chrome extension</a> (DeFi on desktop)</li>
+          <li>Choose restore wallet from seed phrase (same one from MetaMask mobile)</li>
+					<li>Once ETH is cleared to send off Coinbase, send to your wallet address</li>
+					<li>To get address: touch Add Funds on mobile or click account name in Chrome extension</li>
+				</ol>
+        <p class='center'>Now you are ready to try things.</p>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='check-gas-prices'>Check gas prices ⛽</h2>
+				<p class='center'>The network can be busy and cost too much to try things with small amounts<p>
+        <ol>
+					<li>Go to <a href='https://www.gasnow.org/' target='_blank'>GasNow.org</a> or <a href='https://ethgas.app/' target='_blank'>EthGas.app</a></li>
+					<li>This shows gas prices for how fast you want your transactions to be confirmed</li>
+					<li>If all numbers are above 40 or 50, wait for another time when the network is less busy</li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='earn-on-compound'>Try: Earn interest on Compound 📈</h2>
+				<p class='center'>Supply assets to the protocol and earn interest. No minimum. Withdraw anytime.<p>
+        <ol>
+          <li>Go to <a href='https://app.compound.finance' target='_blank'>app.compound.finance</a> in Chrome (with MetaMask extension ready)</li>
+          <li>Click to use MetaMask wallet, approve popup</li>
+					<li>Notice the Supply Markets APY for lending particular assets</li>
+					<li>Take special notice of stablecoin markets such as USDC, DAI, and USDT</li>
+					<li>Stablecoins are designed to always be worth $1 so you can consider them US Dollars in crypto</li>
+					<li>Click on an asset to supply (perhaps you need to <a href='#supply-to-uniswap'>swap</a> for the one you want to supply)</li>
+          <li>Type in amount you wish to supply</li>
+					<li>You may have to click Enable for that asset first, confirm that transaction</li>
+          <li>Click Supply and then confirm the transaction</li>
+					<li>Done. <a href="https://compound.finance/documents/Compound.Whitepaper.pdf" target='_blank'>Read about how compound.finance works</a></li>
+					<li>Or take a short lesson and <a href="https://coinbase.com/earn/comp/invite/5nkp6vrj" target='_blank'>earn $10 of $COMP tokens</a> from Coinbase</li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='swap-on-uniswap'>Try: Swap tokens on Uniswap 🦄</h2>
+				<p class='center'>Swap a token for another token straight from your wallet<p>
+        <ol>
+          <li>Go to <a href='https://app.uniswap.org/#/swap' target='_blank'>app.uniswap.org/#/swap</a> in Chrome (with MetaMask extension ready to go)</li>
+          <li>Click connect wallet, choose MetaMask wallet, click connect</li>
+          <li>Select ETH as the from token</li>
+          <li>Select DAI, or any token you'd like, as the to token</li>
+          <li>Type in amount of ETH (perhaps 0.01)</li>
+          <li>Click to approve new tokens (one time approval per token)</li>
+          <li>Click swap and approve transaction</li>
+          <li>When transaction is complete, token will be in your wallet</li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='track-your-portfolio'>Try: Track your DeFi portfolio 📊</h2>
+				<p class='center'>All you need is your public Ethereum address and they map it all out for you<p>
+        <ol>
+					<li>Get your address by clicking MetaMask extension and click Account to copy public address</li>
+					<li>There are a few options:
+						<ul class='arrow'>
+							<li class='arrow'><a href='https://zapper.fi' target='_blank'>zapper.fi</a> then click to connect wallet or paste in address</li>
+							<li class='arrow'><a href='https://app.zerion.io/connect-wallet' target='_blank'>zerion.io</a> then click to connect wallet or click Addresses to paste in your address</li>
+							<li class='arrow'><a href='https://debank.com/portfolio/new' target='_blank'>debank.com</a> then paste in your address</li>
+						</ul>
+					</li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='play-no-loss-lottery'>Try: Play a no-loss lottery 🎟</h2>
+				<p class='center'>Enter to win all the interest earned on the pool<p>
+        <ol>
+          <li>Go to <a href='https://app.pooltogether.com/' target='_blank'>app.pooltogether.com</a> in Chrome (with MetaMask)</li>
+					<li>Click Account, click Connect Wallet, click Meta Mask, confirm any popups</li>
+					<li>Click Pools and click Get tickets</li>
+					<li>Enter number of tickets. (Need DAI? <a href='#swap-on-uniswap'>Swap for some on Uniswap</a>) Click continue.</li>
+					<li>Here are 2 transactions to confirm: Allow DAI and then Deposit</li>
+					<li>All set! Weekly drawings are Friday evenings. Good luck!</li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='save-with-mstable'>Try: Save with mStable 🧮</h2>
+				<p class='center'>Deposit stablecoins into mStable's high-interest savings contract<p>
+        <ol>
+					<li>Buy some USDC in the amount you want to save on <a href='https://www.coinbase.com/price/usdc' target='_blank'>Coinbase</a> (no fees!)</li>
+					<li>Send the USDC to your Ethereum address (need some ETH too)</li>
+					<li>Go to <a href='https://app.mstable.org/#/save' target='_blank'>mStable.org/save</a>, click Connect and choose MetaMask</li>
+					<li>Click imUSD (stands for interest-bearing mUSD)</li>
+					<li>Choose USDC in the deposit dropdown of popup, then type in amount to deposit</li>
+					<li>Click to Deposit. May have to approve a first transaction to enable USDC</li>
+					<li>Done! Holding imUSD means you're earning interest</li>
+					<li>BONUS: Deposit your imUSD into the imUSD Vault to earn MTA token rewards</li>
+					<li>Explore the numbers behind the APY on <a href='https://app.mstable.org/analytics' target='_blank'>mStable.org/analytics</a></li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='supply-to-uniswap'>Try: Supply liquidity and earn fees on Uniswap 🦄</h2>
+				<p class='center'>Supply liquidity to a trading pool and earn the fees from people paying to swap<p>
+        <ol>
+					<li>Understand what ⚠️<a href='https://finematics.com/impermanent-loss-explained/' target='_blank'>Impermanent Loss</a> ⚠️ is first</li>
+					<li>We will put 2 tokens of equal total USD value into a 50/50 pool for others to swap between</li>
+					<li>A good resource to see which pools are earning the most fees: <a href='https://info.uniswap.org/pairs' target='_blank'>info.uniswap.org/pairs</a></li>
+					<li>A good pool that earns decent APY from fees is the USDC-ETH pool</li>
+					<li>If you have, for example, $50 of USDC and $50 of ETH, you can try the next step</li>
+					<li>Go to <a href='https://app.uniswap.org/#/add/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48/ETH' target='_blank'>app.uniswap.org/add</a></li>
+					<li>Enter $50 for USDC and ETH should prefill with $50 worth of ETH</li>
+					<li>Click to Approve (for USDC), then click Supply. Done!</li>
+					<li>If you don't have 2 tokens to supply, you can <a href='#swap-on-uniswap'>swap for what you need first on Uniswap</a></li>
+					<li>Track your liquidity position on <a href='#track-your-portfolio'>Zapper.fi</a> 📊and <a href='#track-il'>APY.Vision</a> 👀</li>
+        </ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='track-il'>Try: Track impermanent loss on APY.Vision 👀</h2>
+				<p class='center'>Connect or enter your wallet address to get info on your liquidity positions<p>
+        <ol>
+					<li>Go to <a href='https://apy.vision/' target='_blank'>APY.Vision</a></li>
+					<li>Scroll to find and click Connect Wallet</li>
+					<li>Choose Meta Mask, confirm popup</li>
+					<li>You should now see all your liquidity positions from popular protocols</li>
+					<li>"Liquidity Pool Gains" is your $ loss/gain from asset allocation changes being in the pool</li>
+				</ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+        <h2 id='buy-dpi'>Try: Invest in DeFi with a single token - DPI 💲</h2>
+				<p class='center'>DeFi Pulse Index (DPI) is a capitalization-weighted index that tracks top DeFi tokens<p>
+        <ol>
+					<li>Go to <a href='https://www.coinbase.com/' target='_blank'>Coinbase</a> and buy dollar amount of ETH you want of DPI (add $2-$5 for gas)</li>
+					<li>Once the ETH is available to send off Coinbase, send it to your <a href='#get-a-wallet'>wallet</a></li>
+					<li>Go to <a href='https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b' target='_blank'>Uniswap</a> 🦄 to swap the ETH for DPI</li>
+					<li>Enter amount of ETH to swap for DPI, click swap, confirm transaction</li>
+					<li>Done. DPI will be in your wallet once transaction is confirmed</li>
+					<li>Information on the DPI token: <a href='https://www.tokensets.com/portfolio/dpi' target='_blank'>TokenSets.com/portfolio/dpi</a></li>
+				</ol>
+      </div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+      	<p class='center'>Coming soon: Supply liquidity to earn fees and $BAL on Balancer ⚖️</p>
+			</div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <div class='content'>
+      	<p class='center'>Coming soon: Supply and Borrow on Aave 👻</p>
+			</div>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+      <p class='center'>More coming soon 🤓</p>
+      
+      <div class='divider'><div class='divider-line'></div></div>
+      
+    </div>
+
+  </body>
+  
+  <footer>
+  	created by <a href='https://twitter.com/tomuky' target='_blank'>@tomuky</a>
+  </footer>
+  
+</html>
