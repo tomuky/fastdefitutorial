987d9473afa000dcb884097106d8a3ac6f9a8dcf
Tom Davis <tom@Toms-MacBook-Pro.local>
2023-07-05 11:13:20 -0400

    added external link indicator icon

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -5,6 +5,7 @@ const ExternalLink = (props) => {
         <>
             <a href={props.to} target='_blank' rel="noreferrer">
                 {props.title}
+                <img alt='external link' src={require('../images/external-link-icon.png')} className={classes.inline_icon}/>
             </a>
             {props.icon&&<img alt='link icon' src={require(`../images/${props.icon}`)} className={classes.inline_icon}/>}
         </>
