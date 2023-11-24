c6ccfa06880c2db393ceb4191c1e0774cf17a059
Tom Davis <tom@toms-mbp.lan>
2023-11-23 21:25:09 -0500

    update uniswap tenderize link

--- src/topics/earn-on-tenderize.js
+++ src/topics/earn-on-tenderize.js
@@ -20,7 +20,7 @@ const EarnOnTenderize = () => {
                 <Step n='2'>If you're not prompted to switch to the <ExternalLink title="Arbitrum" to="try-arbitrum" icon="arbitrum-logo.png"/> network, click on Wrong Network in top right to switch</Step>
                 <Step n='3'>We will try staking Livepeer tokens (LPT), so first we need to get LPT tokens</Step>
                 <Step type='info'>Livepeer is a decentralized video infrastructure network. Here's a <ExternalLink title='quick primer' to='https://livepeer.org/primer'/></Step>
-                <Step type='info'>Swap for some using <ExternalLink title="app.uniswap.org/swap" to="https://app.uniswap.org/swap?chain=arbitrum" icon="uniswap-logo.png"/> on Arbitrum</Step>
+                <Step type='info'>Swap for some using <ExternalLink title="app.uniswap.org/swap" to="https://app.uniswap.org/swap?chain=arbitrum" icon="uniswap-logo.png"/></Step>
                 <Step n='4'>Once you own some LPT, click on Livepeer in left pane and find an orchestrator in list</Step>
                 <Step type="info">Preferrebly you simply choose one with high APY and decent TVL</Step>
                 <Step n='5'>Type in desired amount to stake, then click Stake, and confirm txs</Step>
