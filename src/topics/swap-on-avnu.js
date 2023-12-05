f33f1563c39cdfc5087f232caf6be729ea31e8cb
Tom Davis <tom@toms-mbp.lan>
2023-12-05 10:34:08 -0500

    add AVNU ref link

--- src/topics/swap-on-avnu.js
+++ src/topics/swap-on-avnu.js
@@ -13,7 +13,7 @@ const SwapOnAvnu = () => {
                 <p>AVNU finds you the best price for your swap on Starknet</p>
             </TopicDescription>
             <TopicBody>
-                <Step n='1'>Go to <ExternalLink to='https://app.avnu.fi/' title='app.avnu.fi' icon='avnu-logo.png'/> and connect your <InternalLink to='/get-braavos' title='Braavos wallet' icon='braavos-logo.png'/></Step>
+                <Step n='1'>Go to <ExternalLink to='https://app.avnu.fi/?referral=0x46b6006ed659db5beb52005efe8d36b8a2393e240e83c3949f6d48af8c90ae5' title='app.avnu.fi' icon='avnu-logo.png'/> and connect your <InternalLink to='/get-braavos' title='Braavos wallet' icon='braavos-logo.png'/></Step>
                 <Step n='2'>Choose the token you wish to swap from and the token you wish to swap into</Step>
                 <Step n='3'>Click Swap and confirm the Braavos wallet prompt</Step>
                 <Step n='4'>Done! AVNU finds you the best price for that swap.</Step>
